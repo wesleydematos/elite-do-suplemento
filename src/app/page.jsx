fea392e0864a2db823c1f586447d5e6d8a329490
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-10-24 10:04:08 -0300

    feat: added sections

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,19 @@ export default function Home() {
   return (
     <main className="bg-body min-h-screen">
       <Header/>
+      <div className="h-[70px]"/>
+      <section className="px-4 py-2 lg:px-32" id="#">
+        <p>inicio</p>
+      </section>
+      <section className="px-4 py-2 lg:px-32" id="Produtos">
+        <p>Produtos</p>
+      </section>
+      <section className="px-4 py-2 lg:px-32" id="Depoimentos">
+        <p>Depoimentos</p>
+      </section>
+      <section className="px-4 py-2 lg:px-32" id="Contato">
+        <p>contatos</p>
+      </section>
     </main>
   )
 }
