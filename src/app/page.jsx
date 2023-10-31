4dcd41210d4b7ef6d377adc01414413cd255f64f
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-10-31 10:10:45 -0300

    refactor: updated padding in components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,17 +4,17 @@ export default function Home() {
   return (
     <main className="bg-body min-h-screen">
       <Header/>
-      <div className="h-[70px]"/>
-      <section className="px-4 py-2 lg:px-32" id="#">
+      <div className="h-[102px]"/>
+      <section className="px-4 lg:px-6 xl:px-32  py-2" id="#">
         <p>inicio</p>
       </section>
-      <section className="px-4 py-2 lg:px-32" id="Produtos">
+      <section className="px-4 lg:px-6 xl:px-32  py-2" id="Produtos">
         <p>Produtos</p>
       </section>
-      <section className="px-4 py-2 lg:px-32" id="Depoimentos">
+      <section className="px-4 lg:px-6 xl:px-32  py-2" id="Depoimentos">
         <p>Depoimentos</p>
       </section>
-      <section className="px-4 py-2 lg:px-32" id="Contato">
+      <section className="px-4 lg:px-6 xl:px-32  py-2" id="Contato">
         <p>contatos</p>
       </section>
     </main>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,8 @@
 "use client"
 
 import { useState } from "react"
-import { FaShoppingCart } from "react-icons/fa"
+import {  HiPhone, HiOutlineTruck } from "react-icons/hi"
+import { FaShoppingCart, FaInstagram } from "react-icons/fa"
 import Image from "next/image"
 import elite from "../../../public/elite.png"
 
@@ -11,7 +12,23 @@ export default function Header() {
 
   return (
     <header className="w-screen bg-white fixed">
-      <nav className="border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+      <div className="bg-secondary w-full h-8 flex px-4 lg:px-6 xl:px-32  justify-between items-center text-white">
+        <div className="flex gap-3">
+          <a className="flex gap-1" href="https://www.instagram.com/elite.dosuplemento/" target="_blank">
+            <FaInstagram />
+            <p className="text-[12px]">elite.dosuplemento </p>
+          </a>
+          <a className="flex gap-1" href="https://wa.me/message/NA4G3HTG6M66H1" target="_blank">
+            <HiPhone/>
+            <p className="text-[12px]">(81) 9 8810-9971</p>
+          </a>
+        </div>
+        <div className="hidden md:flex gap-3">
+          <HiOutlineTruck/>
+          <p className="text-[12px]">Entregamos em seu endereço.</p>
+        </div>
+      </div>
+      <nav className="border-gray-400 border-b-[1px] px-4 lg:px-6 xl:px-32 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <div className="flex items-center gap-2">
             <Image src={elite} width={50} height={50} alt="Elite suplementos logo" />
@@ -24,7 +41,7 @@ export default function Header() {
                   <li key={section}>
                     <a 
                       href={`#${section === "Início" ? "" : section}`} 
-                      className={`block py-2 pr-4 pl-3 font-semibold ${actualSection === section ? "text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" : "text-primary border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"}`}
+                      className={`block py-2 pr-4 pl-3 font-semibold ${actualSection === section ? "text-white rounded bg-secondary lg:bg-transparent lg:text-secondary lg:p-0 dark:text-white" : "text-primary border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-secondary lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"}`}
                       onClick={()=>setActualSection(section)}
                     >
                       {section}
@@ -33,7 +50,7 @@ export default function Header() {
                 )
               })}
               <li>
-                <a href="/carrinho" className="text-primary hover:text-primary-700"><FaShoppingCart/></a>
+                <a href="/carrinho" className="text-primary hover:text-secondary"><FaShoppingCart/></a>
               </li>
             </ul>
           </div>
