540ea80173c306eb2d95dfc26fe33a8500d6b10b
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-11-01 10:03:37 -0300

    style: added google svg to login button

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -14,7 +14,6 @@ export default function Login() {
       <Head>
         <title>Login</title>
       </Head>
-
       <div className="max-w-sm w-full space-y-8">
         <div>
           <Image
@@ -36,21 +35,24 @@ export default function Login() {
         </div>
         <div className="flex flex-col justify-center items-center gap-2">
           {session && session.user.email === "wesleydematos3@gmail.com" && <Link href="/secret/adm" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primary hover:bg-primary-hover focus:outline-none focus:ring-2 focus:ring-offset-2">Ir para o painel</Link>}
-
           {session && session.user.email !== "wesleydematos3@gmail.com" && <p className="text-secondary text-center">Você não possui permissão para acessar o painel.</p>}
-          
           <Link 
             href="/" 
             className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primary hover:bg-primary-hover focus:outline-none focus:ring-2 focus:ring-offset-2">
             Voltar para Home
           </Link>
-
-
           <button
             className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primary hover:bg-primary-hover focus:outline-none focus:ring-2 focus:ring-offset-2"
             onClick={() =>{ session ? signOut() : signIn("google")}}
           >
-            { session ? "Sair" : "Entrar" }
+            { 
+              session ? "Sair" : <>
+                <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
+                  <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd"/>
+                </svg>
+                Entrar com o Google
+              </> 
+            }
           </button>
         </div>
       </div>
