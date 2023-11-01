4f97ef5fb3fa01168ed004ec61d6cb4850f05524
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-11-01 09:49:26 -0300

    feat: changed url domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "www.gsuplementos.com.br", 
+            "lojamaxtitanium.vtexassets.com", 
+            "blackskullusa.vtexassets.com",
+            "images.tcdn.com.br",
+            "integralmedica.vteximg.com.br",
+            "m.media-amazon.com",
+            "acdn.mitiendanube.com",
+            "down-br.img.susercontent.com"
+        ]
+    }
+}
 
 module.exports = nextConfig
