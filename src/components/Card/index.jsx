e5d3c3f3484a840fbf2def901da940762a6bad46
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-11-02 09:50:45 -0300

    feat: updated cards and card list

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,12 +4,12 @@ export default function Card ({name, imageURL, weight, flavor, brand, price}){
   return (
     <div className="flex flex-col px-[25px] bg-white w-[250px] rounded-xl shadow-xl text-primary">
       <div className="h-[200px] flex self-center">
-        <Image width={100} height={100} src={imageURL} alt={name} className="w-[150px] h-[150px] mix-blend-multiply self-center hover:scale-110"/>
+        <Image width={100} height={100} src={imageURL} alt={name} className="w-[150px] h-[150px] self-center hover:scale-125"/>
       </div>
       <p className="max-w-[200px] truncate font-bold">{`${name} ${brand}`}</p>
-      <p className="max-w-[200px] truncate text-sm self-center">{`${weight} - ${flavor}`}</p>
+      <p className="max-w-[200px] truncate text-sm self-center">{`${weight} ${!!flavor ?  "- " + flavor  : ""}`}</p>
       <span className="text-sm self-center">R$ {price},00</span>
-      <button type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-3">
+      <button type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-3">
         Adicionar ao carrinho
       </button>
     </div>
--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,17 +1,77 @@
-import Card from "../Card";
+import Card from "../Card"
+import database from "@/database"
 
 export default function Products (){
   return (
-    <section className="px-4 lg:px-6 xl:px-32 py-2" id="Produtos">
-      <p>Produtos</p>
-      <Card 
-        name="Pré-Treino Horus" 
-        imageURL="https://www.gsuplementos.com.br/upload/growth-layout-personalizado/produto/107/produto-principal.png"
-        weight="150g"
-        flavor="Frutas Vemelhas"
-        brand="Max Titanium"
-        price="70"
-      />
+    <section className="flex flex-col px-4 lg:px-6 xl:px-32 py-2" id="Produtos">
+      <h2 className="font-bold text-xl my-3 border-b-2 border-secondary w-fit">Proteínas</h2>
+      <div className="flex flex-wrap w-full justify-center gap-8">
+        {database.protein.map((item)=>{
+          const {id, name, imageURL, weight, flavor, brand, price} = item
+          return (
+            <Card 
+              key={id}
+              name={name}
+              imageURL={imageURL}
+              weight={weight}
+              flavor={flavor}
+              brand={brand}
+              price={price}
+            />
+          )
+        })}
+      </div>
+      <h2 className="font-bold text-xl my-3 border-b-2 border-secondary w-fit">Pré-Treinos</h2>
+      <div className="flex flex-wrap w-full justify-center gap-8">
+        {database.preWorkout.map((item)=>{
+          const {id, name, imageURL, weight, flavor, brand, price} = item
+          return (
+            <Card 
+              key={id}
+              name={name}
+              imageURL={imageURL}
+              weight={weight}
+              flavor={flavor}
+              brand={brand}
+              price={price}
+            />
+          )
+        })}
+      </div>
+      <h2 className="font-bold text-xl my-3 border-b-2 border-secondary w-fit">Creatinas</h2>
+      <div className="flex flex-wrap w-full justify-center gap-8">
+        {database.creatine.map((item)=>{
+          const {id, name, imageURL, weight, flavor, brand, price} = item
+          return (
+            <Card 
+              key={id}
+              name={name}
+              imageURL={imageURL}
+              weight={weight}
+              flavor={flavor}
+              brand={brand}
+              price={price}
+            />
+          )
+        })}
+      </div>
+      <h2 className="font-bold text-xl my-3 border-b-2 border-secondary w-fit">Outros</h2>
+      <div className="flex flex-wrap w-full justify-center gap-8">
+        {database.others.map((item)=>{
+          const {id, name, imageURL, weight, flavor, brand, price} = item
+          return (
+            <Card 
+              key={id}
+              name={name}
+              imageURL={imageURL}
+              weight={weight}
+              flavor={flavor}
+              brand={brand}
+              price={price}
+            />
+          )
+        })}
+      </div>
     </section>
 )
 }
