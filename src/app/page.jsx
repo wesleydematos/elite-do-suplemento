eb93b2b446ae4ff971ecca5cd526642759e76c8e
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-11-06 09:52:56 -0300

    feat: created productDetails modal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,30 @@
+"use client"
+
 import Header from "@/components/Header"
+import ProductDetail from "@/components/ProductDetail"
 import Products from "@/components/Products"
+import {useProductStore} from "@/store/zustand"
 
 export default function Home() {
+  const {productDetails} = useProductStore()
+
   return (
-    <main className="bg-body min-h-screen">
-      <Header/>
-      <div className="h-[102px]"/>
-      <section className="px-4 lg:px-6 xl:px-32  py-2" id="#">
-        <p>inicio</p>
-      </section>
-      <Products/>
-      <section className="px-4 lg:px-6 xl:px-32  py-2" id="Depoimentos">
-        <p>Depoimentos</p>
-      </section>
-      <section className="px-4 lg:px-6 xl:px-32  py-2" id="Contato">
-        <p>contatos</p>
-      </section>
+    <main className="bg-body min-h-screen flex">
+      <div>
+        <Header/>
+        <div className="h-[102px]"/>
+        <section className="px-4 lg:px-6 xl:px-32  py-2" id="#">
+          <p>inicio</p>
+        </section>
+        <Products/>
+        <section className="px-4 lg:px-6 xl:px-32  py-2" id="Depoimentos">
+          <p>Depoimentos</p>
+        </section>
+        <section className="px-4 lg:px-6 xl:px-32  py-2" id="Contato">
+          <p>contatos</p>
+        </section>
+      </div>
+      {productDetails && <ProductDetail/>}
     </main>
   )
 }
--- src/components/ProductDetail/index.jsx
+++ src/components/ProductDetail/index.jsx
@@ -1,16 +1,27 @@
 import Image from "next/image"
+import {useProductStore} from "@/store/zustand"
 
-export default function ProductDetail({name, imageURL, weight, flavor, brand, price, description, whenToTake}){
+export default function ProductDetail(){
+  const {changeProductDetails, product} = useProductStore()
+  const {name, imageURL, weight, flavor, brand, price, description, whenToTake} = product
+
   return (
-    <aside className="fixed h-full w-full bg-slate-200">
-      <div className="h-[200px] flex self-center">
-        <Image width={100} height={100} src={imageURL} alt={name} className="w-[150px] h-[150px] self-center hover:scale-125"/>
+    <div className="fixed h-full w-full bg-[#00000080] flex items-center justify-center z-40">
+      <div className="flex flex-col w-[90%] h-[95%] p-2 md:p-5 bg-white rounded-lg">
+        <button onClick={changeProductDetails} className="absolute top-[4vh] right-[7vw] font-bold text-secondary">X</button>
+        <div className="h-[150px] md:h-[200px] lg:h-[250px] flex self-center">
+          <Image width={100} height={100} src={imageURL} alt={name} className="md:h-[165px] md:w-[165px] lg:h-[200px] lg:w-[200px] self-center hover:scale-125"/>
+        </div>
+        <p className="font-bold text-center md:text-xl text-secondary">{`${name} ${brand} ${weight}`}</p>
+        <p className="text-sm text-center mb-2 md:mb-4 md:text-xl font-medium">{` ${!!flavor ? flavor + " - " : ""} R$${price},00`}</p>
+        <p className="text-[12px] text-justify mb-2 md:mb-4 md:text-xl"><span className="font-bold text-secondary">Descrição: </span> {description}</p>
+        <p className="text-[12px] text-justify md:text-xl"><span className="font-bold text-secondary">Como Usar: </span>{whenToTake}</p>
+        <button type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-4 md:mt-6 w-[300px] md:w-[500px] md:text-xl self-center" 
+        onClick={console.log("oi")}
+        >
+        Adicionar ao carrinho
+      </button>
       </div>
-      <p className="max-w-[200px] truncate font-bold">{`${name} ${brand}`}</p>
-      <p className="max-w-[200px] truncate text-sm self-center">{`${weight} ${!!flavor ?  "- " + flavor  : ""}`}</p>
-      <span className="text-sm self-center">R$ {price},00</span>
-      <p>{description}</p>
-      <p>{whenToTake}</p>
-    </aside>
+    </div>
   )
 }
