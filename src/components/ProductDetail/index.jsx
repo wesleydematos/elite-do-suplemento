bc9dd2ec5313e899fda8b95d2732b8be6f2d2424
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-11-09 09:30:22 -0300

    feat: created function addToCart

--- src/components/ProductDetail/index.jsx
+++ src/components/ProductDetail/index.jsx
@@ -1,9 +1,25 @@
 import Image from "next/image"
 import {useProductStore} from "@/store/zustand"
+import {toast} from "react-toastify"
 
 export default function ProductDetail(){
   const {changeProductDetails, product} = useProductStore()
   const {name, imageURL, weight, flavor, brand, price, description, whenToTake} = product
+
+  function addToCart(){
+    const products = localStorage.getItem("productsCart")
+
+    if(!products){
+      localStorage.setItem("productsCart", JSON.stringify([{...product}]))
+    } else {
+      const newProducts = JSON.parse(products)
+      newProducts.push({...product})
+
+      localStorage.setItem("productsCart", JSON.stringify(newProducts))
+    }
+
+    toast.success("Item adicionado ao carrinho!")
+  }
 
   return (
     <div className="fixed h-full w-full bg-[#00000080] flex items-center justify-center z-40">
@@ -17,7 +33,7 @@ export default function ProductDetail(){
         <p className="text-[12px] text-justify mb-2 md:mb-4 md:text-xl"><span className="font-bold text-secondary">Descrição: </span> {description}</p>
         <p className="text-[12px] text-justify md:text-xl"><span className="font-bold text-secondary">Como Usar: </span>{whenToTake}</p>
         <button type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-4 md:mt-6 w-[300px] md:w-[500px] md:text-xl self-center" 
-        onClick={console.log("oi")}
+        onClick={addToCart}
         >
         Adicionar ao carrinho
       </button>
