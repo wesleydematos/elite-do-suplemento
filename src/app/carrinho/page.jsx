ecce7e0fb02ec231017891c209d1aca0680101c1
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-11-09 10:29:24 -0300

    feat: recover shopping cart

--- src/app/carrinho/page.jsx
+++ src/app/carrinho/page.jsx
@@ -1,7 +1,26 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
 export default function Carrinho() {
+  const [products, setProducts] = useState([])
+  const [productsChanged, setProductsChanged] = useState(false)
+
+  useEffect(()=>{
+    const allProducts = localStorage.getItem("productsCart")
+    setProducts(JSON.parse(allProducts))
+  }, [productsChanged])
+
+
     return (
-      <main className="bg-body min-h-screen">
-        <p>carrinho</p>
+      <main className="bg-body min-h-screen flex flex-col">
+        <h1 className="font-bold text-xl md:text-5xl self-center my-3 text-primary">MEU CARRINHO DE COMPRAS</h1>
+        {
+          !!products ? 
+          <>tem produto</> 
+          : 
+          <>n tem produto</>
+        }
       </main>
     )
   }
