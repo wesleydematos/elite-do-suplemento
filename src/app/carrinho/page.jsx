8a28fd5ce3c9c67070abf914b54d5b6ee966c28d
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-11-10 08:47:22 -0300

    feat: empty cart

--- src/app/carrinho/page.jsx
+++ src/app/carrinho/page.jsx
@@ -1,6 +1,10 @@
 "use client"
 
+import Image from "next/image"
+import logo from "../../../public/elite.png"
 import { useEffect, useState } from "react"
+import { FaShoppingCart } from "react-icons/fa"
+import { BiArrowBack } from "react-icons/bi"
 
 export default function Carrinho() {
   const [products, setProducts] = useState([])
@@ -14,13 +18,28 @@ export default function Carrinho() {
 
     return (
       <main className="bg-body min-h-screen flex flex-col">
-        <h1 className="font-bold text-xl md:text-5xl self-center my-3 text-primary">MEU CARRINHO DE COMPRAS</h1>
-        {
-          !!products ? 
-          <>tem produto</> 
-          : 
-          <>n tem produto</>
-        }
+        <div className="bg-secondary h-[25px] text-white flex justify-center items-center gap-2">
+          <FaShoppingCart/>
+          <p className="text-xs">MEU CARRINHO</p>
+        </div>
+        <header className="bg-white shadow-md w-full flex items-center justify-center h-[75px]">
+          <Image src={logo} width={50} height={50} alt="logo elite suplementos"/>
+          <h1 className="font-semibold ml-3 text-xl md:text-3xl text-primary">ELITE SUPLEMENTOS</h1>
+        </header>
+        <section className="flex flex-col items-start px-4 lg:px-6 xl:px-32 py-2 mt-2">
+          <a href="/" className="p-2 rounded-md text-white bg-primary flex items-center gap-2">
+            <BiArrowBack/> Continuar comprando
+          </a>
+          {
+            !!products ? 
+            <div className="mt-2">tem produto</div> 
+            : 
+            <div className="mt-5 flex flex-col self-center text-primary text-center gap-3">
+              <FaShoppingCart className="text-8xl w-[300px]"/>
+              <p className="text-xl font-bold w-[300px]">SEU CARRINHO ESTÁ VAZIO</p>
+            </div> 
+          }
+        </section>
       </main>
     )
   }
