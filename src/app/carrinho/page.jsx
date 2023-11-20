8f443c0532c3efac94fd13452016360893821a0b
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-11-20 09:51:56 -0300

    feat: added loading

--- src/app/carrinho/page.jsx
+++ src/app/carrinho/page.jsx
@@ -2,15 +2,16 @@
 
 import Image from "next/image"
 import logo from "../../../public/elite.png"
-import { useEffect, useState } from "react"
-import { FaShoppingCart } from "react-icons/fa"
-import { FaUpLong, FaDownLong } from "react-icons/fa6"
-import { BiArrowBack } from "react-icons/bi"
+import {useEffect, useState} from "react"
+import {FaShoppingCart} from "react-icons/fa"
+import {FaUpLong, FaDownLong} from "react-icons/fa6"
+import {BiArrowBack} from "react-icons/bi"
 
 export default function Carrinho() {
   const [products, setProducts] = useState([])
   const [total, setTotal] = useState(0)
   const [redirectURL, setRedirectURL] = useState(false)
+  const [loading, setLoading] = useState(true)
 
   function removeProduct(id){
     const newProducts = products.filter((item)=> item.id !== id)
@@ -70,7 +71,7 @@ export default function Carrinho() {
 
       text += `totalizando R$${totalPrice},00.`
     }
-    
+
     return `https://wa.me/+558188109971?text=${text}` 
   }
 
@@ -84,6 +85,10 @@ export default function Carrinho() {
     
     let text = "Olá, estou entrando em contato através do site para solicitar "
 
+    if(allProducts.length == 1){
+      text += `(${allProducts[0].quantity}) ${allProducts[0].name} da ${allProducts[0].brand} que totalizou R$${allProducts[0].quantity * allProducts[0].price},00.`
+    }
+
     if(allProducts.length > 1){
       text  += ` os produtos `
       allProducts.forEach((item)=>{
@@ -91,8 +96,6 @@ export default function Carrinho() {
       })
 
       text += `totalizando R$${totalSum},00.`
-    }else{
-      text += `(${allProducts[0].quantity}) ${allProducts[0].name} da ${allProducts[0].brand} que totalizou R$${allProducts[0].quantity * allProducts[0].price},00.`
     }
     
 
@@ -101,6 +104,7 @@ export default function Carrinho() {
     setProducts(allProducts)
     setTotal(totalSum)
     setRedirectURL(url)
+    setLoading(false)
   }, [])
 
 
@@ -114,87 +118,92 @@ export default function Carrinho() {
           <Image src={logo} width={50} height={50} alt="logo elite suplementos"/>
           <h1 className="font-semibold ml-3 text-xl md:text-2xl text-primary">ELITE SUPLEMENTOS</h1>
         </header>
-        <section className="flex flex-col items-start px-4 lg:px-6 xl:px-32 py-2 mt-2">
-          <a href="/" className="p-2 rounded-md text-white bg-primary flex items-center gap-2">
-            <BiArrowBack/> Continuar comprando
-          </a>
-          {
-            !!products.length ? 
-            <div className="mt-2 w-full">
-              <ul className="my-4 flex flex-col gap-3 justify-center">
-                {products.map((product)=>{
-                  return (
-                    <li key={product.id} className="flex flex-col md:flex-row w-full bg-white rounded-xl p-2 shadow-lg md:gap-[0.15rem] md:justify-between">
-                      <Image 
-                        width={100} 
-                        height={100} 
-                        src={product.imageURL} 
-                        alt={product.name} 
-                        className="mix-blend-multiply self-center mb-2"
-                      />
-                      <p className="self-center font-semibold md:w-[130px] md:text-center lg:w-[300px] lg:text-start">{`${product.name} ${product.brand}`}</p>
-                      <p className="self-center font-semibold mb-2 md:w-[100px] lg:w-[200px] md:text-center lg:text-start">{`${product.weight} ${!!product.flavor ?  "- " + product.flavor  : ""}`}</p>
-                      <div className="flex w-[full] justify-between md:self-center md:gap-[5px] lg:min-w-[250px]">
-                        <div className="flex flex-col">
-                          <p className=" font-semibold">Unidade:</p>
-                          <span>R${product.price},00</span>
-                        </div>
-                        <div className="flex flex-col">
-                          <p className=" font-semibold">Quant:</p>
-                          <div className="flex gap-1 self-center w-[52px] justify-between">
-                            <button 
-                              disabled={product.quantity === 10}
-                              className={`${product.quantity === 10 && "text-gray-400"} text-[12px] `}
-                              onClick={() => changeQuantity(product.id, "increase")}
-                            >
-                              <FaUpLong/>
-                            </button>
-                            <span className="w-[18px] text-center">{product.quantity}</span>
-                            <button 
-                              disabled={product.quantity === 1}
-                              className={`${product.quantity === 1 && "text-gray-400"} text-[12px] `}
-                              onClick={() => changeQuantity(product.id, "decrease")}
-                            >
-                              <FaDownLong/>
-                            </button>
+        {
+          loading ? 
+          <div className="self-center mt-5 font-bold text-xl text-primary">Carregando...</div>
+          :
+          <section className="flex flex-col items-start px-4 lg:px-6 xl:px-32 py-2 mt-2">
+            <a href="/" className="p-2 rounded-md text-white bg-primary flex items-center gap-2">
+              <BiArrowBack/> Continuar comprando
+            </a>
+            {
+              !!products.length ? 
+              <div className="mt-2 w-full">
+                <ul className="my-4 flex flex-col gap-3 justify-center">
+                  {products.map((product)=>{
+                    return (
+                      <li key={product.id} className="flex flex-col md:flex-row w-full bg-white rounded-xl p-2 shadow-lg md:gap-[0.15rem] md:justify-between">
+                        <Image 
+                          width={100} 
+                          height={100} 
+                          src={product.imageURL} 
+                          alt={product.name} 
+                          className="mix-blend-multiply self-center mb-2"
+                        />
+                        <p className="self-center font-semibold md:w-[130px] md:text-center lg:w-[300px] lg:text-start">{`${product.name} ${product.brand}`}</p>
+                        <p className="self-center font-semibold mb-2 md:w-[100px] lg:w-[200px] md:text-center lg:text-start">{`${product.weight} ${!!product.flavor ?  "- " + product.flavor  : ""}`}</p>
+                        <div className="flex w-[full] justify-between md:self-center md:gap-[5px] lg:min-w-[250px]">
+                          <div className="flex flex-col">
+                            <p className=" font-semibold">Unidade:</p>
+                            <span>R${product.price},00</span>
+                          </div>
+                          <div className="flex flex-col">
+                            <p className=" font-semibold">Quant:</p>
+                            <div className="flex gap-1 self-center w-[52px] justify-between">
+                              <button 
+                                disabled={product.quantity === 10}
+                                className={`${product.quantity === 10 && "text-gray-400"} text-[12px] `}
+                                onClick={() => changeQuantity(product.id, "increase")}
+                              >
+                                <FaUpLong/>
+                              </button>
+                              <span className="w-[18px] text-center">{product.quantity}</span>
+                              <button 
+                                disabled={product.quantity === 1}
+                                className={`${product.quantity === 1 && "text-gray-400"} text-[12px] `}
+                                onClick={() => changeQuantity(product.id, "decrease")}
+                              >
+                                <FaDownLong/>
+                              </button>
+                            </div>
+                          </div>
+                          <div className="flex flex-col w-[82px]">
+                            <p className=" font-semibold">Total:</p>
+                            <p>R${product.price * product.quantity},00</p>
                           </div>
                         </div>
-                        <div className="flex flex-col w-[82px]">
-                          <p className=" font-semibold">Total:</p>
-                          <p>R${product.price * product.quantity},00</p>
-                        </div>
-                      </div>
-                      <button 
-                        className="text-white mt-2 bg-accent rounded-md w-1/2 self-center hover:scale-95 md:w-[110px] lg:mr-1"
-                        onClick={() => removeProduct(product.id)}
-                      >
-                        Remover
-                      </button>
-                    </li>
-                  )
-                })}
-              </ul>
-              {
-              products && 
-                <>
-                  <p className="text-xl font-bold my-2 text-primary">Total: R${total},00</p>
-                  <a 
-                    href={redirectURL} 
-                    target="_blank" 
-                    className={`rounded-md text-white bg-primary flex items-center gap-2 w-fit py-2 px-5 ${redirectURL ? "" : "cursor-not-allowed bg-slate-400  "}`}
-                  >
-                    Solicitar pedido
-                  </a>
-                </>
-              }
-            </div> 
-            : 
-            <div className="mt-16 flex flex-col self-center text-primary text-center gap-3">
-              <FaShoppingCart className="text-8xl w-[300px]"/>
-              <p className="text-xl font-bold w-[300px]">SEU CARRINHO ESTÁ VAZIO</p>
-            </div> 
-          }
-        </section>
+                        <button 
+                          className="text-white mt-2 bg-accent rounded-md w-1/2 self-center hover:scale-95 md:w-[110px] lg:mr-1"
+                          onClick={() => removeProduct(product.id)}
+                        >
+                          Remover
+                        </button>
+                      </li>
+                    )
+                  })}
+                </ul>
+                {
+                products && 
+                  <>
+                    <p className="text-xl font-bold my-2 text-primary">Total: R${total},00</p>
+                    <a 
+                      href={redirectURL} 
+                      target="_blank" 
+                      className={`rounded-md text-white bg-primary flex items-center gap-2 w-fit py-2 px-5 ${redirectURL ? "" : "cursor-not-allowed bg-slate-400  "}`}
+                    >
+                      Solicitar pedido
+                    </a>
+                  </>
+                }
+              </div> 
+              : 
+              <div className="mt-16 flex flex-col self-center text-primary text-center gap-3">
+                <FaShoppingCart className="text-8xl w-[300px]"/>
+                <p className="text-xl font-bold w-[300px]">SEU CARRINHO ESTÁ VAZIO</p>
+              </div> 
+            }
+          </section>
+        }
       </main>
     )
   }
