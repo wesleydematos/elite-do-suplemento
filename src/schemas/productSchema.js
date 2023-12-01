4e9551be268f1e99817e48232c60379cf05f4dfb
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-12-01 10:27:10 -0300

    fix: adjusted product schema

--- src/schemas/productSchema.js
+++ src/schemas/productSchema.js
@@ -6,12 +6,38 @@ export const productSchema = yup.object().shape({
   .required("Nome obrigatório!"),
   imageURL: yup
   .string()
-  .required("Url da imagem obrigatória!"),
+  .required("Url da imagem obrigatória!")
+  .test('isValidDomain', 'Domínio não permitido!', function (value){
+    const domainRegex = /^(?:https?:\/\/)?(?:www\.)?([^\/]+)/
+    const match = value.match(domainRegex)
+
+    if (!match || !match[1]) {
+      return false
+    }
+
+    const allowedDomains = [
+      "www.gsuplementos.com.br",
+      "lojamaxtitanium.vtexassets.com",
+      "blackskullusa.vtexassets.com",
+      "images.tcdn.com.br",
+      "integralmedica.vteximg.com.br",
+      "m.media-amazon.com",
+      "acdn.mitiendanube.com",
+      "down-br.img.susercontent.com",
+      "a-static.mlcdn.com.br",
+      "d2r9epyceweg5n.cloudfront.net",
+      "i.imgur.com"
+    ]
+
+    return allowedDomains.includes(match[1])
+  }),
   weight: yup
   .string()
   .required("Peso/Medida obrigatório!"),
   flavor: yup
-  .string(),
+  .string()
+  .default("")
+  .nullable(),
   description: yup
   .string()
   .max(680, "Máximo de 680 caractéres!")
@@ -26,4 +52,7 @@ export const productSchema = yup.object().shape({
   price: yup
   .string()
   .required("Preço obrigatório!"),
+  type: yup
+  .string()
+  .required("Tipo obrigatório!")
 })
