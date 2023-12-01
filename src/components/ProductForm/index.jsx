dc17f58caad5ef40741828da638978f5e3a6307d
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-12-01 10:28:24 -0300

    feat: created function to add new product

--- src/components/ProductForm/index.jsx
+++ src/components/ProductForm/index.jsx
@@ -1,8 +1,9 @@
 import BlueButton from "../BlueButton"
-// import axios from "axios"
+import {productSchema} from "@/schemas/productSchema"
+import axios from "axios"
 import {useForm} from "react-hook-form"
 import {yupResolver} from "@hookform/resolvers/yup"
-import {productSchema} from "@/schemas/productSchema"
+import {toast} from "react-toastify"
 
 export default function ProductForm(){
   const {
@@ -12,24 +13,21 @@ export default function ProductForm(){
   } = useForm({resolver: yupResolver(productSchema)})
 
   async function handleCreate(data) {
-    console.log(data)
-
-    // try {
-    //   await axios.post("/user", data)
-    //   toast.success("Produto adicionado com sucesso!")
-    //   navigate("/");
-    // } catch (error) {
-    //   console.log(error)
-    //   toast.error("Erro ao adicionar produto!")
-    // } 
+    try {
+      await axios.post("/api/product", data)
+      toast.success("Produto adicionado com sucesso!")
+    } catch (error) {
+      console.log(error)
+      toast.error("Erro ao adicionar produto!")
+    } 
   }
 
   return (
     <>
-      <h2 className="font-bold text-xl my-3 border-b-2 border-secondary w-fit">Adicionar produto:</h2>
+      <h2 className="font-bold text-xl my-3 border-b-2 border-secondary w-fit">Adicionar novo produto:</h2>
       <form onSubmit={handleSubmit(handleCreate)}>
-        <div className="flex flex-col">
-          <label htmlFor="name">Nome</label>
+        <div className="flex flex-col mb-2">
+          <label className="font-medium text-primary" htmlFor="name">Nome</label>
           <input 
             {...register("name")}
             errors={errors.name}
@@ -37,10 +35,10 @@ export default function ProductForm(){
             placeholder="Whey Protein" 
             id="name"
           />
-          {errors.name && <span className="text-accent">{errors.name.message}</span>}
+          {errors.name && <span className="text-secondary">{errors.name.message}</span>}
         </div>
-        <div className="flex flex-col">
-          <label htmlFor="imageURL">URL da imagem</label>
+        <div className="flex flex-col mb-2">
+          <label className="font-medium text-primary" htmlFor="imageURL">URL da imagem</label>
           <input 
             {...register("imageURL")}
             errors={errors.imageURL}
@@ -48,10 +46,10 @@ export default function ProductForm(){
             placeholder="https://..." 
             id="imageURL"
           />
-          {errors.imageURL && <span className="text-accent">{errors.imageURL.message}</span>}
+          {errors.imageURL && <span className="text-secondary">{errors.imageURL.message}</span>}
         </div>
-        <div className="flex flex-col">
-          <label htmlFor="weight">Peso/Medida</label>
+        <div className="flex flex-col mb-2">
+          <label className="font-medium text-primary" htmlFor="weight">Peso/Medida</label>
           <input 
             {...register("weight")}
             errors={errors.weight}
@@ -59,10 +57,10 @@ export default function ProductForm(){
             placeholder="150g" 
             id="weight"
           />
-          {errors.weight && <span className="text-accent">{errors.weight.message}</span>}
+          {errors.weight && <span className="text-secondary">{errors.weight.message}</span>}
         </div>
-        <div className="flex flex-col">
-          <label htmlFor="flavor">Sabor</label>
+        <div className="flex flex-col mb-2">
+          <label className="font-medium text-primary" htmlFor="flavor">Sabor</label>
           <input 
             {...register("flavor")}
             errors={errors.flavor}
@@ -70,10 +68,10 @@ export default function ProductForm(){
             placeholder="Frutas vermelhas" 
             id="flavor"
           />
-          {errors.flavor && <span className="text-accent">{errors.flavor.message}</span>}
+          {errors.flavor && <span className="text-secondary">{errors.flavor.message}</span>}
         </div>
-        <div className="flex flex-col">
-          <label htmlFor="description">Descrição</label>
+        <div className="flex flex-col mb-2">
+          <label className="font-medium text-primary" htmlFor="description">Descrição</label>
           <input 
             {...register("description")}
             errors={errors.description}
@@ -81,10 +79,10 @@ export default function ProductForm(){
             placeholder="O produto contém..." 
             id="description"
           />
-          {errors.description && <span className="text-accent">{errors.description.message}</span>}
+          {errors.description && <span className="text-secondary">{errors.description.message}</span>}
         </div>
-        <div className="flex flex-col">
-          <label htmlFor="whenToTake">Quando utilizar</label>
+        <div className="flex flex-col mb-2">
+          <label className="font-medium text-primary" htmlFor="whenToTake">Quando utilizar</label>
           <input 
             {...register("whenToTake")}
             errors={errors.whenToTake}
@@ -92,10 +90,10 @@ export default function ProductForm(){
             placeholder="O produto deve ser utilizado..." 
             id="whenToTake"
           />
-          {errors.whenToTake && <span className="text-accent">{errors.whenToTake.message}</span>}
+          {errors.whenToTake && <span className="text-secondary">{errors.whenToTake.message}</span>}
         </div>
-        <div className="flex flex-col">
-          <label htmlFor="brand">Marca</label>
+        <div className="flex flex-col mb-2">
+          <label className="font-medium text-primary" htmlFor="brand">Marca</label>
           <input 
             {...register("brand")}
             errors={errors.brand}
@@ -103,10 +101,10 @@ export default function ProductForm(){
             placeholder="Growth" 
             id="brand"
           />
-          {errors.brand && <span className="text-accent">{errors.brand.message}</span>}
+          {errors.brand && <span className="text-secondary">{errors.brand.message}</span>}
         </div>
-        <div className="flex flex-col">
-          <label htmlFor="price">Preço</label>
+        <div className="flex flex-col mb-2">
+          <label className="font-medium text-primary" htmlFor="price">Preço</label>
           <input 
             {...register("price")}
             errors={errors.price}
@@ -114,10 +112,25 @@ export default function ProductForm(){
             placeholder="150" 
             id="price"
           />
-          {errors.price && <span className="text-accent">{errors.price.message}</span>}
+          {errors.price && <span className="text-secondary">{errors.price.message}</span>}
+        </div>
+        <div className="flex flex-col mb-2">
+          <label className="font-medium text-primary" htmlFor="type">Tipo</label>
+          <select 
+            {...register("type")}
+            errors={errors.type}
+            name="type" 
+            id="type" 
+          >
+            <option value="preWorkout">Pré-treino</option>
+            <option value="protein">Proteína</option>
+            <option value="creatine">Creatina</option>
+            <option value="others">Outros</option>
+          </select>
+          {errors.type && <span className="text-secondary">{errors.type.message}</span>}
         </div>
         <div className="my-2">
-          <BlueButton type="submit">Criar</BlueButton>
+          <BlueButton type="submit">Adicionar</BlueButton>
         </div>
       </form>
     </>
